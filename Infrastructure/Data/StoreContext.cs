e49718808f18fdc7211cb31b6a66bb5401f07d36
Grandhe_Gayathri <Grandhe_Gayathri@Dell.com>
2022-02-01 14:05:25 +0530

    intializedirectory

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ WebApplication5/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Controllers
+{  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        public StoreContext _storeContext { get; }
+
+        public ProductController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Products>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+    }
+}
